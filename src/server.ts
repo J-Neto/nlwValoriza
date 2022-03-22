e95162bda71a2f12d07d765d65e06e56ad829b44
José <ribeirojoseph44@gmail.com>
2022-03-22 10:36:16 -0300

    Primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,23 @@ import express from "express";
 // @types/express
 const app = express();
 
+/**
+ * GET    => Buscar uma informação
+ * POST   => Inserir (Criar) uma informação
+ * PUT    => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH  => Alterar uma informação específica
+ */
+
+app.get('/test', (req, res) => {
+    // Request => Entrando
+    // Response => Saindo
+    return res.send('Hello World');
+});
+
+app.post('/test-post', (req, res) => {
+    return res.send('Hello World POST')
+});
+
 // http://localhost:3000
 app.listen(3000, () => console.log('Server is running'));
