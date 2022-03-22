a862e4479dd653fe36e9929fdb73638626965ed2
José <ribeirojoseph44@gmail.com>
2022-03-22 11:31:24 -0300

    Att de comentarios

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,16 @@ const app = express();
  * PATCH  => Alterar uma informação específica
  */
 
+/**
+ * Tipos de parâmetros
+ * Routes Params => http://localhost:3000/produtos/3435689
+ * Query Params => http://localhost:3000/produtos?name=teclado&description=tecladobom&...
+ * Body Params => {
+ *  "name": "teclado",
+ *  "description": "teclado bom"
+ * }
+ */
+
 app.get('/test', (req, res) => {
     // Request => Entrando
     // Response => Saindo
