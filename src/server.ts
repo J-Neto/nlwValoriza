e0a94eccd0d9daf3e7083b055bc39a59d05efb01
José <ribeirojoseph44@gmail.com>
2022-03-23 15:44:07 -0300

    Funcionalidades da tag

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
-import { router } from "./routes";
 import "express-async-errors";
+import { router } from "./routes";
 
 import "./database";
 
@@ -15,13 +15,13 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     if (err instanceof Error) {
         return response.status(400).json({
             error: err.message,
-        })
+        });
     }
 
     return response.status(500).json({
         status: "error",
         message: "Internal Server Error",
-    })
+    });
 });
 
-app.listen(3000, () => console.log('Server is running'));
+app.listen(3000, () => console.log("Server is running"));
