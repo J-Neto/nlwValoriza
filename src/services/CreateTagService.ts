06f4927e48a6588a6b12f69da56b62e6dedafecd
José <ribeirojoseph44@gmail.com>
2022-04-01 09:05:56 -0300

    Commit de praxe

--- src/services/CreateTagService.ts
+++ src/services/CreateTagService.ts
@@ -1,62 +1,30 @@
-// import { getCustomRepository } from "typeorm";
-// import { TagsRepositories } from "../repositories/TagsRepositories";
-
-// class CreateTagService {
-//     async execute(name: string) {
-//         const tagRepositories = getCustomRepository(TagsRepositories);
-        
-//         if (!name) {
-//             throw new Error("Incorrect name!");
-//         }
-        
-//         const tagAlreadyExists = await tagRepositories.findOne({
-//             name,
-//         })
-
-//         if (tagAlreadyExists) {
-//             throw new Error("Tag already exists!");
-//         }
-
-//         const tag = tagRepositories.create({
-//             name
-//         })
-
-//         await tagRepositories.save(tag);
-
-//         return tag;
-//     }
-// }
-
-// export { CreateTagService }
-
 import { getCustomRepository } from "typeorm";
 import { TagsRepositories } from "../repositories/TagsRepositories";
 
 class CreateTagService {
-  async execute(name: string) {
-    const tagsRepositories = getCustomRepository(TagsRepositories);
+    async execute(name: string) {
+        const tagRepositories = getCustomRepository(TagsRepositories);
+        
+        if (!name) {
+            throw new Error("Incorrect name!");
+        }
+        
+        const tagAlreadyExists = await tagRepositories.findOne({
+            name,
+        })
 
-    if (!name) {
-      throw new Error("Incorrect name!");
+        if (tagAlreadyExists) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagRepositories.create({
+            name
+        })
+
+        await tagRepositories.save(tag);
+
+        return tag;
     }
-
-    // SELECT * FROM TAGS WHERE NAME = 'name'
-    const tagAlreadyExists = await tagsRepositories.findOne({
-      name,
-    });
-
-    if (tagAlreadyExists) {
-      throw new Error("Tag already exists!");
-    }
-
-    const tag = tagsRepositories.create({
-      name,
-    });
-
-    await tagsRepositories.save(tag);
-
-    return tag;
-  }
 }
 
-export { CreateTagService };
+export { CreateTagService }
